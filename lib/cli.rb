08f3778548acf6ae4b03a54c5b19462edf04b0d5
idmyn <hello@davidmyno.rs>
2019-09-30 17:35:56 +0100

    added methods for register/login; began methods for review deletion

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,31 +1,49 @@
+# coding: utf-8
 class CLI
   @@prompt = TTY::Prompt.new
 
   def run
-    return if greeting.eql?("No")
+    response = greeting
 
-    @user = login
-    main_menu
-    choice = choose_restaurant
-    if choice.eql?("Add a new restaurant")
-      new_restaurant = create_restaurant
-      write_review(new_restaurant.name)
-    else
-      write_review(choice)
+    @user = response.eql?("Log in") ? login : register
+
+    p @user
+
+    menu_selection = main_menu
+    if menu_selection.eql?("Review a restaurant")
+      choice = choose_restaurant
+      if choice.eql?("Add a new restaurant")
+        new_restaurant = create_restaurant
+        write_review(new_restaurant.name)
+      else
+        write_review(choice)
+      end
+    elsif menu_selection.eql?("Delete one of your reviews")
+      delete_review
     end
   end
 
   def greeting
-    puts "Hello!"
-    @@prompt.select("Looking for food?", %w[Yes No])
+    @@prompt.select("Hello!", "Log in", "Register")
+  end
+
+  def register
+    email = @@prompt.ask("Please enter your email address:")
+    name = @@prompt.ask("What should we call you?")
+    User.create(name: name, email: email)
   end
 
   def login
-    User.new(name: @@prompt.ask("What should we call you?"))
+    email = @@prompt.ask("Please enter your email address:")
+    User.find_by(email: email)
+    # check if a user was actually found
+    # if not, ask them to register or add a different email
   end
 
   def main_menu
+    # options = ["Review a restaurant", "Delete one of your reviews"]
     options = ["Review a restaurant"]
+    options << "Delete one of your reviews" unless @user.restaurants.empty?
     @@prompt.select("How can we help you today?", options)
   end
 
@@ -43,8 +61,26 @@ class CLI
     content = @@prompt.ask("Please write a review:")
 
     Review.create(
-      # add user id
-      rating: rating, content: content, restaurant_id: restaurant.id
+      rating: rating,
+      content: content,
+      restaurant_id: restaurant.id,
+      user_id: @user.id
     )
+  end
+
+  def delete_review
+    choice = choose_user_review
+    puts choice
+    #review details are shown
+    #"are you sure?"
+    #delete
+  end
+
+  def choose_user_review
+    restaurant_name =
+      @@prompt.select("Which review do you want to delete?", @user.restaurant_names)
+    #list all of the user’s reviews
+    #user can choose a review
+    Restaurant.find_by(name: restaurant_name)
   end
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :reviews
   has_many :restaurants, through: :reviews
+
+  def restaurant_names
+    restaurants.map(&:name)
+  end
 end
