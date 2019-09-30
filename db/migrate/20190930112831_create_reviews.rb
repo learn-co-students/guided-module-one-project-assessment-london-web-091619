9b90257a8e38b44f1798be2647b8f99426a035e6
idmyn <hello@davidmyno.rs>
2019-09-30 14:14:00 +0100

    fix consistent use of ‘user’ rather than ‘customer’

--- db/migrate/20190930112831_create_reviews.rb
+++ db/migrate/20190930112831_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
       t.integer :restaurant_id
-      t.integer :customer_id
+      t.integer :user_id
       t.float :rating
       t.string :content
 
