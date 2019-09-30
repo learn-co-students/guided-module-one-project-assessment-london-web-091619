5feef595fc4ad461cc4fa8866c70004450704c53
idmyn <hello@davidmyno.rs>
2019-09-30 16:44:47 +0100

    add methods for creating restaurant/review

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,15 +2,22 @@ class CLI
   @@prompt = TTY::Prompt.new
 
   def run
-    greeting
+    return if greeting.eql?("No")
+
     @user = login
     main_menu
-    choose_restaurants.eql?("Add a new restaurant") ? create_restaurant : write_review
+    choice = choose_restaurant
+    if choice.eql?("Add a new restaurant")
+      new_restaurant = create_restaurant
+      write_review(new_restaurant.name)
+    else
+      write_review(choice)
+    end
   end
 
   def greeting
     puts "Hello!"
-    # @@prompt.select("Looking for food?", %w[Yes No])
+    @@prompt.select("Looking for food?", %w[Yes No])
   end
 
   def login
@@ -18,11 +25,26 @@ class CLI
   end
 
   def main_menu
-    options = ["Show me restaurants"]
+    options = ["Review a restaurant"]
     @@prompt.select("How can we help you today?", options)
   end
 
-  def choose_restaurants
+  def choose_restaurant
     @@prompt.select("x", Restaurant.random_names(10), "Add a new restaurant")
+  end
+
+  def create_restaurant
+    Restaurant.create(name: @@prompt.ask("What is the restaurant called?"))
+  end
+
+  def write_review(restaurant_name)
+    restaurant = Restaurant.find_by(name: restaurant_name)
+    rating = @@prompt.ask("How many stars would you give #{restaurant.name}? (out of 5)")
+    content = @@prompt.ask("Please write a review:")
+
+    Review.create(
+      # add user id
+      rating: rating, content: content, restaurant_id: restaurant.id
+    )
   end
 end
