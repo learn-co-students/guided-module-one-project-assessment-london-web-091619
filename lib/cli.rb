c3f3ef87df1a647e4da5a06a60e3573ccda8819a
idmyn <hello@davidmyno.rs>
2019-10-02 14:58:47 +0100

    enable restaurant login, begin read_customer_reviews

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,25 +4,37 @@ class CLI
 
   def run
     puts "Hello!"
-    @user = login
-    main_menu
+    user_type = find_user_type
+
+    @user = user_type.eql?("Customer") ? user_login : restaurant_login
+    @user.class.eql?(User) ? user_menu : restaurant_menu
   end
 
-  def login
+  def find_user_type
+    @@prompt.select("Are you a customer or a restaurant?", "Customer", "Restaurant")
+  end
+
+  def user_login
     email = @@prompt.ask("Please enter your email address:")
     return User.find_by(email: email) if User.find_by(email: email)
 
-    failure_message = "We couldn't find a user with that email address. Would you like to try again or register a new account?"
+    failure_message = "We couldn't find a customer account with that email address. Would you like to try again or register a new account?"
     response = @@prompt.select(failure_message, "Try again", "Register new account")
-    response.eql?("Try again") ? login : register(email)
+
+    response.eql?("Register new account") ? register_user(email) : login
   end
 
-  def register(email)
+  def restaurant_login
+    restaurant_name = @@prompt.select("Which restaurant would you like to log in as?", Restaurant.random_names(10))
+    Restaurant.find_by(name: restaurant_name)
+  end
+
+  def register_user(email)
     name = @@prompt.ask("What should we call you?")
     User.create(name: name, email: email)
   end
 
-  def main_menu
+  def user_menu
     refresh_user
 
     options = ["Search for a restaurant", "Review a restaurant"]
@@ -32,8 +44,22 @@ class CLI
     menu_selection(selection)
   end
 
+  def restaurant_menu
+    options = ["Read reviews", "Exit"]
+    selection = @@prompt.select("Hi #{@user.name}, how can we help you today?", options)
+    read_customer_reviews if selection.eql?("Read reviews")
+      puts "Thank you for using our app!"
+  end
+
   def refresh_user
     @user = User.find(@user.id)
+  end
+
+  def read_customer_reviews
+    # list reviews for this restaurant
+    puts @user.reviews_for_prompt
+    @@prompt.select("", "Exit")
+    puts "Thank you for using our app!"
   end
 
   def menu_selection(selection)
@@ -72,7 +98,7 @@ class CLI
     lat_long = latitude + "," + longitude
 
     choice = @@prompt.select("What would you like to do?", "Get directions", "Go back to main menu")
-    choice.eql?("Get directions") ? get_directions(lat_long) : main_menu
+    choice.eql?("Get directions") ? get_directions(lat_long) : user_menu
   end
 
   def get_directions(lat_long)
@@ -83,7 +109,7 @@ class CLI
     end
     puts ""
     @@prompt.select("", "Thanks")
-    main_menu
+    user_menu
   end
 
   def review_restaurant
@@ -96,11 +122,11 @@ class CLI
       write_review(choice)
     end
 
-    main_menu
+    user_menu
   end
 
   def choose_restaurant
-    @@prompt.select("x", Restaurant.random_names(10), "Add a new restaurant")
+    @@prompt.select("", Restaurant.random_names(10), "Add a new restaurant")
   end
 
   def create_restaurant
@@ -122,11 +148,12 @@ class CLI
 
   def update_review
     chosen_review = choose_user_review("Which review would you like to update?")
+    # default value should be previous value
     chosen_review.rating = @@prompt.slider("Rating", max: 5, min: 0, step: 0.5, default: 2.5, format: "|:slider| %.1f")
     chosen_review.content = @@prompt.ask("Please write a new review:")
     chosen_review.save
 
-    main_menu
+    user_menu
   end
 
   def delete_review
@@ -137,7 +164,7 @@ class CLI
       chosen_review.destroy
     end
 
-    main_menu
+    user_menu
   end
 
   def choose_user_review(message)
