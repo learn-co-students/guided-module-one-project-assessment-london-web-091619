1eb6a74aae281c678882fe95f55411ee4f789121
idmyn <hello@davidmyno.rs>
2019-10-04 09:18:14 +0100

    change single quotes to double quotes for consistency

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
-require 'bundler'
+require "bundler"
 Bundler.require
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.sqlite')
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: "db/development.sqlite")
 
 ActiveRecord::Base.logger = nil
 
-require_all 'lib'
+require_all "lib"
